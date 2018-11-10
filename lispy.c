51af58d70b1866f91ad0c4dafe0512261e60a248
Jacek Kubicki <jacnik@gmail.com>
2018-11-10 21:51:35 +0100

    added test code that prints evaluated tree

--- lispy.c
+++ lispy.c
@@ -32,12 +32,39 @@ long eval(mpc_ast_t* t)
 
   /* Iterate the remaining children and combine */
   int i = 3;
-  while (strstr(t->children[i]->tag, "expr")) {
+  while (strstr(t->children[i]->tag, "expr")) 
+  {
     x = eval_op(x, op, eval(t->children[i]));
     i++;
   }
 
   return x;
+}
+
+void test(mpc_ast_t* t) 
+{
+/*
+typedef struct mpc_ast_t {
+	  char* tag;
+	  char* contents;
+	  mpc_state_t state;
+	  int children_num;
+	  struct mpc_ast_t** children;
+} mpc_ast_t;
+*/
+
+  printf("Tag: %s\n", t->tag);
+  printf("Contents: %s\n", t->contents);
+  printf("Number of children: %i\n", t->children_num);
+
+  for (int i = 0; i < t->children_num; i++)
+  {
+    printf("\n Child at %i:\n", i);
+    mpc_ast_t* child = t->children[i];
+    printf("Tag: %s\n", child->tag);
+    printf("Contents: %s\n", child->contents);
+    printf("Number of children: %i\n", child->children_num);
+  }
 }
 
 int main(int argc, char** argv) 
@@ -77,6 +104,8 @@ int main(int argc, char** argv)
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r )) 
     {
+	test(r.output); // todo remove
+
       /* If parsed correctly evaluate and print */
       long result = eval(r.output);
       printf("%li\n", result);
