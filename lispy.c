314f7209d17124e84077f9acefd32d0ae4af5575
Jacek Kubicki <jacnik@gmail.com>
2018-11-10 19:28:33 +0100

    expression evaluation added

--- lispy.c
+++ lispy.c
@@ -7,8 +7,41 @@
 #include "external/mpc.h"
 
 
-int main(int argc, char** argv) {
-  
+long eval_op(long x, char* op, long y)
+{
+  if (strcmp(op, "+") == 0) {return x + y;}
+  if (strcmp(op, "-") == 0) {return x - y;}
+  if (strcmp(op, "*") == 0) {return x * y;}
+  if (strcmp(op, "/") == 0) {return x / y;}
+  return 0;
+}
+
+long eval(mpc_ast_t* t) 
+{
+  /* If tagged as number return it directly  */
+  if(strstr(t->tag, "number"))
+  {
+    return atoi(t->contents);
+  }
+
+  /* The operator is always second child */
+  char* op = t->children[1]->contents;
+
+  /* Store the third child in 'x' */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combine */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
+int main(int argc, char** argv) 
+{
   /* Create parser */
   mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
@@ -32,8 +65,8 @@ int main(int argc, char** argv) {
   puts("Press Ctrl+c to Exit\n");
 
   /* in a never ending loop */
-  while(1) {
-    
+  while(1) 
+  {  
     /* Output the prompt and get input */
     char* input = readline("lispy> ");
 
@@ -42,11 +75,15 @@ int main(int argc, char** argv) {
 
     /* Attempt to parse the user Input */
     mpc_result_t r;
-    if (mpc_parse("<stdin>", input, Lispy, &r )) {
-      /* On success Print the AST */
-      mpc_ast_print(r.output);
+    if (mpc_parse("<stdin>", input, Lispy, &r )) 
+    {
+      /* If parsed correctly evaluate and print */
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
-    } else {
+    } 
+    else 
+    {
       /* Otherwise Print the Error */
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
